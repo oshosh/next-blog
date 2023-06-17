c9e077b076737424f5540dd17bb996502b22cac9
oshosh <dhtpgus7@gmail.com>
2023-06-18 00:43:43 +0900

    feat: blog 마크다운 적용2

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -10,16 +10,16 @@ interface Props {
 async function PostPage({ params: { slug } }: Props) {
   const getData = await getMarkdown(slug);
 
-  if (!getData) null;
-
-  return (
-    <>
-      <div className='bg-gray-100 p-9 rounded-md'>
-        <div>{getData?.id}</div>
-        <Md markdown={getData?.md?.content}></Md>
-      </div>
-    </>
-  );
+  if (!getData) return;
+  else
+    return (
+      <>
+        <div className='bg-gray-100 p-9 rounded-md'>
+          <div>{getData?.id}</div>
+          <Md markdown={getData?.md?.content}></Md>
+        </div>
+      </>
+    );
 }
 
 export default PostPage;
--- src/components/md/Md.tsx
+++ src/components/md/Md.tsx
@@ -2,6 +2,7 @@
 
 import { GrayMatterFile } from 'gray-matter';
 import Image from 'next/image';
+import { Components } from 'react-markdown';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
@@ -12,33 +13,35 @@ interface Props {
 }
 
 function Md({ markdown }: Props) {
+  const components: Components = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || '');
+
+      return !inline && match ? (
+        <SyntaxHighlighter language={match[1]} PreTag='div' {...props} style={materialDark}>
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      ) : (
+        <code {...props}>{children}</code>
+      );
+    },
+    img: (image) => (
+      <Image
+        className='object-cover h-72'
+        src={image.src || ''}
+        alt={image.alt || ''}
+        width={9999}
+        height={200}
+      />
+    ),
+  };
+
   return (
     <ReactMarkdown
       children={markdown}
       className='prose lg:prose-xl w-full max-w-screen-2xl mx-auto'
       remarkPlugins={[remarkGfm]}
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || '');
-
-          return !inline && match ? (
-            <SyntaxHighlighter language={match[1]} PreTag='div' {...props} style={materialDark}>
-              {String(children).replace(/\n$/, '')}
-            </SyntaxHighlighter>
-          ) : (
-            <code {...props}>{children}</code>
-          );
-        },
-        img: (image) => (
-          <Image
-            className='object-cover h-72'
-            src={image.src || ''}
-            alt={image.alt || ''}
-            width={9999}
-            height={200}
-          />
-        ),
-      }}
+      components={components}
     />
   );
 }
